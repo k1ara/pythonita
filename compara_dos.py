b1d3985ae1d537109388c8fdba6dbe75cf9b28b2
Kiara Arosemena <lynmaru@gmail.com>
2016-02-12 12:06:44 -0500

    arreglado problemas de sintaxis, ya funciona y compara dos numeros

--- compara_dos.py
+++ compara_dos.py
@@ -1,9 +1,9 @@
-#comparar dos números - Indicar el mayor de ellos.
+#comparar dos numeros - Indicar el mayor de ellos.
 
-N1 = int(raw_input("Indicar el primer número: "))
-N2 = int(raw_input("Indicar el segundo número: "))
+N1 = int(raw_input("Indicar el primer numero: "))
+N2 = int(raw_input("Indicar el segundo numero: "))
 
 if N1 > N2:
-	print str(N1)" "+"es mayor que"+" "str(N2)
-else
-	print str(N2)" "+"es menor que"+" "str(N1)
+	print str(N1) + " es mayor que " + str(N2)
+else:
+	print str(N2) + " es menor que " + str(N1)
