0099cf89ae4f95017222dde3daed93a348f2347f
Kiara Arosemena <lynmaru@gmail.com>
2016-02-12 11:57:25 -0500

    agregado condicional IF

--- compara_dos.py
+++ compara_dos.py
@@ -2,3 +2,8 @@
 
 N1 = int(raw_input("Indicar el primer número: "))
 N2 = int(raw_input("Indicar el segundo número: "))
+
+if N1 > N2:
+	print str(N1)" "+"es mayor que"+" "str(N2)
+else
+	print str(N2)" "+"es menor que"+" "str(N1)
